68c3eba41733b0a7b68dcdc930c347a8761ebccb
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-06-22 22:42:27 -0400

    Added user input to the main java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+	Scanner userInput = new Scanner(System.in);
+	Scanner userInputNext = new Scanner(System.in);
+
+	Systems.out.println("Enter your name");
+	
+	String name = myObj.nextLine();
+	System.out.println("Name is: " + userInput);
 
     }
 }
